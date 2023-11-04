99d724b5cf6cd3f7756a086aebaad58b231b8693
Adi <subramanianaditya42@gmail.com>
2023-11-04 18:08:56 +0100

    Unit test for parsing

--- src/logic/movetree/pgn.rs
+++ src/logic/movetree/pgn.rs
@@ -5,38 +5,81 @@ use nom::character::complete::multispace1;
 use nom::character::complete::u8;
 use nom::IResult;
 
-fn parse_move_text(input: &str) -> IResult<&str, &str> {
-    let (input, move_number) = u8(input)?;
-    dbg!(input);
-    dbg!(move_number);
-    let (input, _) = tag(".")(input)?;
-
-    dbg!(input);
-    // take_until1(|c: char| c.is_alphabetic())(input)?;
-    let (rest, input) = take_while1(|c| c == ' ')(input)?;
-    dbg!(rest);
-    // let (rest, whitespace) = multispace1(rest)?;
-    let (rest, white_move) = take_while1(|c: char| c.is_alphanumeric())(rest)?;
-
-    let (rest, whitespace) = multispace1(rest)?;
-    let (rest, black_move) = take_while1(|c: char| c.is_alphanumeric())(rest)?;
-
-    dbg!(black_move);
-    dbg!(white_move);
-    Ok((input, rest))
+#[derive(Debug, PartialEq)]
+struct ParsedMove<'a> {
+    black_move: &'a str,
+    white_move: &'a str,
+    variations: Option<Box<ParsedMove<'a>>>,
+    comment: Option<&'a str>,
 }
 
+impl<'a> ParsedMove<'a> {
+    fn new(black_move: &'a str, white_move: &'a str) -> ParsedMove<'a> {
+        ParsedMove {
+            black_move,
+            white_move,
+            variations: None,
+            comment: None,
+        }
+    }
+}
+
+fn parse_move_text(input: &str) -> IResult<&str, ParsedMove> {
+    let (input, move_number) = u8(input.trim_start())?;
+    let (input, _) = tag(".")(input)?;
+
+    let (rest, input) = take_while1(|c| c == ' ')(input)?;
+    let (rest, white_move) = take_while1(|c: char| c.is_alphanumeric())(rest)?;
+
+    let (rest, _) = multispace1(rest)?;
+    let (rest, black_move) = take_while1(|c: char| c.is_alphanumeric())(rest)?;
+
+    dbg!((black_move, white_move));
+    Ok((rest, ParsedMove::new(black_move, white_move)))
+}
+
+fn parse_pgn(input: &str) -> Vec<ParsedMove> {
+    let mut moves = vec![];
+    let mut left_to_parse = input;
+    while !input.is_empty() {
+        match parse_move_text(left_to_parse) {
+            Ok((rest, parsed_move)) => {
+                moves.push(parsed_move);
+                left_to_parse = rest;
+            }
+            Err(_) => {
+                break;
+            }
+        }
+    }
+
+    moves
+}
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
     fn parses_first_move() {
-        let _ = parse_move_text("1. e4 e5 2. d4 d5");
-    }
-
-    #[test]
-    fn parses_any_move_number() {
-        let _ = parse_move_text("2. d4 d5");
+        let res = parse_pgn("1. e4 e5 2. d4 d5");
+        assert_eq!(res.len(), 2);
+        assert_eq!(
+            res[0],
+            ParsedMove {
+                white_move: "e4",
+                black_move: "e5",
+                comment: None,
+                variations: None,
+            }
+        );
+        assert_eq!(
+            res[1],
+            ParsedMove {
+                white_move: "d4",
+                black_move: "d5",
+                comment: None,
+                variations: None,
+            }
+        );
     }
 }
