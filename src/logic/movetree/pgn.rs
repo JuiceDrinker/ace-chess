f32debe9cacb77862f636e243634b4b6108be6c4
Adi <subramanianaditya42@gmail.com>
2023-11-04 20:47:40 +0100

    Implement parsing of basic PGN structure

--- src/logic/movetree/pgn.rs
+++ src/logic/movetree/pgn.rs
@@ -1,48 +1,52 @@
-#[allow(unused)]
+use nom::branch::alt;
 use nom::bytes::complete::tag;
+use nom::bytes::complete::take_while;
 use nom::bytes::complete::take_while1;
-use nom::character::complete::multispace1;
 use nom::character::complete::u8;
 use nom::IResult;
+use nom::Parser;
 
 #[derive(Debug, PartialEq)]
 struct ParsedMove<'a> {
-    black_move: &'a str,
-    white_move: &'a str,
+    r#move: &'a str,
     variations: Option<Box<ParsedMove<'a>>>,
     comment: Option<&'a str>,
 }
 
 impl<'a> ParsedMove<'a> {
-    fn new(black_move: &'a str, white_move: &'a str) -> ParsedMove<'a> {
+    fn new(r#move: &'a str) -> ParsedMove<'a> {
         ParsedMove {
-            black_move,
-            white_move,
+            r#move,
             variations: None,
             comment: None,
         }
     }
 }
 
-fn parse_move_text(input: &str) -> IResult<&str, ParsedMove> {
-    let (input, move_number) = u8(input.trim_start())?;
+fn parse_move_white(input: &str) -> IResult<&str, ParsedMove> {
+    let (input, _) = u8(input.trim_start())?;
     let (input, _) = tag(".")(input)?;
 
-    let (rest, input) = take_while1(|c| c == ' ')(input)?;
-    let (rest, white_move) = take_while1(|c: char| c.is_alphanumeric())(rest)?;
+    let (rest, _) = take_while(|c| c == ' ')(input)?;
+    let (rest, r#move) = take_while1(|c: char| c.is_alphanumeric()).parse(rest)?;
 
-    let (rest, _) = multispace1(rest)?;
-    let (rest, black_move) = take_while1(|c: char| c.is_alphanumeric())(rest)?;
-
-    dbg!((black_move, white_move));
-    Ok((rest, ParsedMove::new(black_move, white_move)))
+    Ok((rest, ParsedMove::new(r#move)))
 }
 
-fn parse_pgn(input: &str) -> Vec<ParsedMove> {
+fn parse_move_black(input: &str) -> IResult<&str, ParsedMove> {
+    let (rest, r#move) = take_while(|c: char| c.is_alphanumeric()).parse(input.trim_start())?;
+    Ok((rest, ParsedMove::new(r#move)))
+}
+
+fn parse_pgn(input: &str) -> IResult<&str, ParsedMove> {
+    alt((parse_move_white, parse_move_black))(input)
+}
+
+fn parse_game(input: &str) -> Vec<ParsedMove> {
     let mut moves = vec![];
     let mut left_to_parse = input;
-    while !input.is_empty() {
-        match parse_move_text(left_to_parse) {
+    while !left_to_parse.is_empty() {
+        match parse_pgn(left_to_parse) {
             Ok((rest, parsed_move)) => {
                 moves.push(parsed_move);
                 left_to_parse = rest;
@@ -61,13 +65,12 @@ mod test {
 
     #[test]
     fn parses_first_move() {
-        let res = parse_pgn("1. e4 e5 2. d4 d5");
-        assert_eq!(res.len(), 2);
+        let res = parse_game("1. e4 e5 2. d4 d5");
+        assert_eq!(res.len(), 4);
         assert_eq!(
             res[0],
             ParsedMove {
-                white_move: "e4",
-                black_move: "e5",
+                r#move: "e4",
                 comment: None,
                 variations: None,
             }
@@ -75,8 +78,23 @@ mod test {
         assert_eq!(
             res[1],
             ParsedMove {
-                white_move: "d4",
-                black_move: "d5",
+                r#move: "e5",
+                comment: None,
+                variations: None,
+            }
+        );
+        assert_eq!(
+            res[2],
+            ParsedMove {
+                r#move: "d4",
+                comment: None,
+                variations: None,
+            }
+        );
+        assert_eq!(
+            res[3],
+            ParsedMove {
+                r#move: "d5",
                 comment: None,
                 variations: None,
             }
