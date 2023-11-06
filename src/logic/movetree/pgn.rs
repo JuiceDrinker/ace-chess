435d4c11c117793d607fb09e837779050bfd1d4c
Adi <subramanianaditya42@gmail.com>
2023-11-06 18:04:46 +0100

    Implement parsers for chess moves

--- src/logic/movetree/pgn.rs
+++ src/logic/movetree/pgn.rs
@@ -2,10 +2,14 @@
 use nom::{
     branch::alt,
     bytes::complete::{tag, take_until, take_while1},
-    character::complete::{digit1, multispace1, u8},
+    character::{
+        complete::{digit1, multispace1, u8},
+        is_alphabetic,
+    },
     combinator::{map_res, opt, recognize, verify},
+    error::{self, Error, ErrorKind},
     sequence::{delimited, tuple, Tuple},
-    IResult, Parser,
+    Err, IResult, Parser,
 };
 
 #[derive(Debug, PartialEq)]
@@ -26,42 +30,46 @@ impl<'a> ParsedMove<'a> {
 }
 
 fn parse_rank(input: &str) -> IResult<&str, &str> {
-    // dbg!(input);
-    take_while1(|c: char| matches!(c, '1'..='8')).parse(input)
-    // dbg!();
-    // let (a, b) = map_res(digit1, |s: &str| s.parse::<u8>())(input)?;
-
-    // Ok((a, a))
-    // Ok()
+    dbg!("rank", input);
+    dbg!("HEUIFJWEIFJIOWE");
+    digit1.parse(input)
 }
 
 fn parse_file(input: &str) -> IResult<&str, &str> {
-    dbg!(input);
-    take_while1(|c: char| matches!(c, 'a'..='h')).parse(input)
+    dbg!("file", input);
+    // is_alphabetic(input)
+    let (rest, file) = take_while1(|c: char| matches!(c, 'a'..='h')).parse(input)?;
+
+    Ok((rest, file))
 }
 fn parse_piece(input: &str) -> IResult<&str, &str> {
-    dbg!(input);
+    dbg!("piece", input);
     take_while1(|c: char| matches!(c, 'N' | 'B' | 'R' | 'Q' | 'K')).parse(input)
 }
 
-// fn parse_disambiguated_capture(input: &str) -> IResult<&str, &str> {
-//     recognize(tuple((parse_file, tag("x")))).parse(input)
-// }
-fn parse_capture(input: &str) -> IResult<&str, &str> {
-    dbg!(input);
-    tag("x").parse(input)
-    // alt((tag("x"), parse_disambiguated_capture)).parse(input)
+fn parse_disambiguated_capture(input: &str) -> IResult<&str, &str> {
+    recognize(tuple((parse_file, tag("x")))).parse(input)
 }
 
+fn parse_capture(input: &str) -> IResult<&str, &str> {
+    alt((tag("x"), parse_disambiguated_capture)).parse(input)
+}
+
+fn parse_full_capture(input: &str) -> IResult<&str, &str> {
+    recognize(tuple((parse_piece, parse_capture, parse_file, parse_rank))).parse(input)
+}
+
+fn parse_basic(input: &str) -> IResult<&str, &str> {
+    recognize(tuple((parse_piece, parse_file, parse_rank))).parse(input)
+}
+
+fn parse_disambugated_move(input: &str) -> IResult<&str, &str> {
+    recognize(tuple((parse_piece, parse_file, parse_file, parse_rank))).parse(input)
+}
+
+// Nd5, Nbd5, Nbxd5, Nxd5
 fn parse_move_text(input: &str) -> IResult<&str, &str> {
-    recognize(tuple((
-        parse_piece,
-        opt(parse_file),
-        opt(parse_capture),
-        parse_file,
-        parse_rank,
-    )))
-    .parse(input)
+    alt((parse_basic, parse_disambugated_move, parse_full_capture)).parse(input)
 }
 fn parse_move_number_black(input: &str) -> IResult<&str, &str> {
     let (rest, (move_number, tag)) = (u8, tag("... ")).parse(input)?;
@@ -91,7 +99,7 @@ fn parse_pgn(input: &str) -> IResult<&str, ParsedMove> {
     alt((parse_move_white, parse_move_black))(input)
 }
 
-fn parse_game(input: &str) -> Result<Vec<ParsedMove>, nom::Err<nom::error::Error<&str>>> {
+fn parse_game(input: &str) -> Result<Vec<ParsedMove>, nom::Err<Error<&str>>> {
     let mut moves = vec![];
     let mut left_to_parse = input;
     while !left_to_parse.is_empty() {
@@ -112,53 +120,55 @@ mod test {
         assert_eq!(move_text, "Nd5")
     }
 
-    // #[test]
+    #[test]
     fn parses_move_text_with_disambiguation() {
         let (_, move_text) = parse_move_text("Nbd5").unwrap();
         assert_eq!(move_text, "Nbd5")
     }
 
-    // #[test]
+    #[test]
     fn parses_move_text_with_capture() {
         let (_, move_text) = parse_move_text("Nxd5").unwrap();
         assert_eq!(move_text, "Nxd5")
     }
 
-    // #[test]
+    #[test]
     fn parses_capture() {
         let (_, move_text) = parse_capture("xe5").unwrap();
         assert_eq!(move_text, "x")
     }
 
-    // #[test]
+    #[test]
     fn parses_move_text_with_disambiguated_capture() {
         let (_, move_text) = parse_move_text("Nexd5").unwrap();
         assert_eq!(move_text, "Nexd5")
     }
 
-    // #[test]
-    // fn parses_disambiguated_capture() {
-    //     let (_, move_text) = parse_disambiguated_capture("ex").unwrap();
-    //     assert_eq!(move_text, "ex")
-    // }
-    // #[test]
+    #[test]
+    fn parses_disambiguated_capture() {
+        let (_, move_text) = parse_disambiguated_capture("ex").unwrap();
+        assert_eq!(move_text, "ex")
+    }
+
+    #[test]
     fn parses_comments() {
         let (_, comment) = parse_comments("{This is a comment}").unwrap();
         assert_eq!(comment, "This is a comment");
     }
-    // #[test]
+
+    #[test]
     fn parses_move_number() {
         let res = parse_move_number_white("1.").unwrap();
         assert_eq!(res, ("", (1, ".")));
     }
 
-    // #[test]
+    #[test]
     fn parses_any_move_number() {
         let res = parse_move_number_white("8.").unwrap();
         assert_eq!(res, ("", (8, ".")));
     }
 
-    // #[test]
+    #[test]
     fn parses_whites_move() {
         let res = parse_move_white("1.e4").unwrap();
         assert_eq!(
@@ -174,7 +184,7 @@ mod test {
         );
     }
 
-    // #[test]
+    #[test]
     fn parses_black_move() {
         let res = parse_move_black(" e5").unwrap();
         assert_eq!(
@@ -190,7 +200,7 @@ mod test {
         );
     }
 
-    // #[test]
+    #[test]
     fn parses_blacks_move_with_move_number() {
         let res = parse_move_black("2... e5").unwrap();
         assert_eq!(
@@ -206,13 +216,13 @@ mod test {
         );
     }
 
-    // #[test]
+    #[test]
     #[should_panic]
     fn should_panic_for_invalid_pgn() {
         parse_game("1 .e4").unwrap();
     }
 
-    // #[test]
+    #[test]
     fn parses_first_move() {
         let res = parse_game("1.e4 e5 2.d4 d5").unwrap();
         assert_eq!(res.len(), 4);
